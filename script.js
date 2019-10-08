010c30a6b7b7be94b599605b7875bd9ec43d9c45
a-delmas <aurore.delmas@hotmail.fr>
2019-10-08 11:46:28 +0200

    Switch 1

--- script.js
+++ script.js
@@ -4,24 +4,35 @@ document.body.appendChild(play_area);
 play_area.id = "play_area";
 //Fin zone de jeux
 //
-pos_x = (Math.random() * 500);
-pos_y = (Math.random() * 500);
+pos_x = Math.random() * 475;
+pos_y = Math.random() * 475;
 var duck = document.createElement("div");
 play_area.appendChild(duck);
 duck.style.left = pos_x + "px";
 duck.style.top = pos_y + "px";
 duck.id = "duck";
 
-
-setInterval(function () {
-    pos_x = (Math.random() * 475);
-    pos_y = (Math.random() * 475);
+function killduck(event) {
+    console.log(event)
+    duck.remove();
+    
+    
+    duck = document.createElement("div");
+    duck.id = "duck";
+    ppos_x = Math.random() * 475;
+    pos_y = Math.random() * 475;
     duck.style.left = pos_x + "px";
     duck.style.top = pos_y + "px";
-    duck.style.transitionDuration = "2s"
+    play_area.appendChild(duck);
+    duck.addEventListener("click",  killduck);
+}
+
+duck.addEventListener("click",  killduck);
+
+setInterval(function () {
+    pos_x = Math.random() * 475;
+    pos_y = Math.random() * 475;
+    duck.style.left = pos_x + "px";
+    duck.style.top = pos_y + "px";
+    duck.style.transitionDuration = "1s"
 }, 1000);
-
-duck.addEventListener("click", function(event){
-    console.log(event)
-})
-
