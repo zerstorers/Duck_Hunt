4b914f1af19ddac5c6210493da5d263263e07dd7
zerstorers <lemaire-maxime@hotmail.fr>
2019-10-08 12:29:35 +0200

    switch 2

--- script.js
+++ script.js
@@ -4,35 +4,41 @@ document.body.appendChild(play_area);
 play_area.id = "play_area";
 //Fin zone de jeux
 //
-pos_x = Math.random() * 475;
-pos_y = Math.random() * 475;
-var duck = document.createElement("div");
-play_area.appendChild(duck);
-duck.style.left = pos_x + "px";
-duck.style.top = pos_y + "px";
-duck.id = "duck";
+
+
+for (let i = 0; i < 10; i++) {
+    pos_x = Math.random() * 450;
+    pos_y = Math.random() * 200;
+    var duck = document.createElement("div");
+    play_area.appendChild(duck);
+    duck.style.left = pos_x + "px";
+    duck.style.top = pos_y + "px";
+    duck.id = "duck";
+
+}
 
 function killduck(event) {
     console.log(event)
     duck.remove();
-    
-    
+
+
     duck = document.createElement("div");
     duck.id = "duck";
-    ppos_x = Math.random() * 475;
-    pos_y = Math.random() * 475;
+    ppos_x = Math.random() * 450;
+    pos_y = Math.random() * 200;
     duck.style.left = pos_x + "px";
     duck.style.top = pos_y + "px";
     play_area.appendChild(duck);
-    duck.addEventListener("click",  killduck);
+    duck.addEventListener("click", killduck);
 }
 
-duck.addEventListener("click",  killduck);
+duck.addEventListener("click", killduck);
+
 
 setInterval(function () {
-    pos_x = Math.random() * 475;
-    pos_y = Math.random() * 475;
+    pos_x = Math.random() * 450;
+    pos_y = Math.random() * 200;
     duck.style.left = pos_x + "px";
     duck.style.top = pos_y + "px";
-    duck.style.transitionDuration = "1s"
+    duck.style.transitionDuration = "5s"
 }, 1000);
