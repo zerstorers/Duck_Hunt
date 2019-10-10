d7d107337ba8ddd8911f73a1b3dcfa7384b89946
a-delmas <aurore.delmas@hotmail.fr>
2019-10-10 16:53:24 +0200

    Switch 2

--- Duck Hunt 2/script.js
+++ Duck Hunt 2/script.js
@@ -1,39 +1,46 @@
 var play_area = document.createElement("div");
 document.body.appendChild(play_area);
 play_area.id = "play_area";
+// var cpt = 0
 
-function kill_ducks() {
-    this.remove()
+function create_duck () {
     var duck = document.createElement("div");
     play_area.appendChild(duck);
+    duck.className = "ducks";
+
+    return duck
+}
+
+function position_duck(duck) {
     pos_x = Math.random() * 450;
     pos_y = Math.random() * 450;
     duck.style.marginTop = pos_x + "px";
     duck.style.marginLeft = pos_y + "px";
-    duck.className = "ducks";
+}
+
+function kill_ducks() {
+    this.remove()
+    // cpt = cpt+1;
+    var duck = create_duck();
+    position_duck(duck);
     duck.addEventListener("click", kill_ducks)
 }
 
 
 for (let i = 0; i < 15; i++) {
-    pos_x = Math.random() * 450;
-    pos_y = Math.random() * 450;
-    var duck = document.createElement("div");
-    play_area.appendChild(duck);
-    duck.style.marginTop = pos_x + "px";
-    duck.style.marginLeft = pos_y + "px";
-    duck.className = "ducks";
-    duck.addEventListener("click", kill_ducks);
-    
+    var duck = create_duck();
+    position_duck(duck);
+    duck.addEventListener("click", kill_ducks); 
 }
 
 setInterval(function () {
     var all_ducks = document.getElementsByClassName("ducks")
     for (let j = 0; j < all_ducks.length; j++) {
-        pos_x = Math.random() * 450;
-        pos_y = Math.random() * 450;
-        all_ducks[j].style.marginTop = pos_x + "px";
-        all_ducks[j].style.marginLeft = pos_y + "px";
-        all_ducks[j].style.transitionDuration = "3s";
+        position_duck(all_ducks[j]);
+        all_ducks[j].style.transitionDuration = "5s";
     }
-}, 5000);
+}, 3000);
+
+// var compteur = document.createElement("div");
+// document.body.appendChild(compteur);
+// compteur.className = "cpt";
